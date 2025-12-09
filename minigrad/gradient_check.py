c5e279b87d9478ec0fe12f85a6e46b5777a3360c
TuLa <78714490+TuLa-1031@users.noreply.github.com>
2025-12-09 08:55:31 +0700

    update

--- minigrad/gradient_check.py
+++ minigrad/gradient_check.py
@@ -5,6 +5,57 @@ from past.builtins import xrange
 import numpy as np
 from random import randrange
 
+def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
+    """
+    a naive implement of numerical gradient of f at x
+    - f should be a function that takes a single argument
+    - x is the point (numpy array) to evaluate the gradient at
+    """
+
+    fx = f(x) # evaluate funciton value at original point
+    grad = np.zeros_like(x)
+    # iterate over all indexes in x
+    it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
+    while not it.finished:
+
+        # evaluate funciton at x+h
+        ix = it.multi_index
+        oldval = x[ix]
+        x[ix] = oldval + h # increment by h
+        fxph = f(x)
+        x[ix] = oldval - h
+        fxmh = f(x) # evaluate f(x-h)
+        x[i] = oldval # restore
+
+        #compute the partial derivative with centred formula
+        grad[ix] = (fxph - fxmh) / ( 2 * h ) # the slope
+        if verbose:
+            print(ix, grad[ix])
+        it.iternext() # step to next dimension
+
+    return grad
+
+def eval_numerical_gradient_array(f, x, df, h=1e-5):
+    """
+    Evaluate a numeric gradient for a function that accepts a numpy
+    array and returns a numpy array.
+    """
+    grad = np.zeros_like(x)
+    it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
+    while not it.finished:
+        ix = it.multi_index
+
+        oldval = x[ix]
+        x[ix] - oldval + h
+        pos = f(x).copy()
+        x[ix] = oldval - h
+        neg = f(x).copy()
+        x[ix] = oldval
+
+        grad[ix] = np.sum((pos - neg) * df) / (2 * h)
+        it.iternext()
+    return grad
+
 def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
     """
     sample a few random elements and only return numerical in this dimensions.
---
+++ minigrad/optim.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+"""
+This file implements various first-order update rules that are commonly used
+for training neural networks. Each update rule accepts current weights and the
+gradient of the loss with respect to those weights and produces the next set of
+weights. Each update rule has the same interface:
+
+def update(w, dw, config=None):
+
+Inputs:
+    - w: A numpy array giving the current weights.
+    - dw: A numpy array of the shape as w giving the gradient of the
+      loss with respect to w.
+    - config: A dictionary containing hyperparameter values such as learning
+      rate, momentum, etc. If the update rule requires caching values over many
+      iteraions, then config will also hold these caches values.
+
+Returns:
+    - next_w: The next point after the update.
+    - config: The config dictionary to be passed to the next iteration of the
+      update rule.
+
+NOTE: for most udpate rules, the defaults learning rate will probably not
+perform well; howeeer the defaults values of the other hyperparameters should
+work well for a variety of different problems.
+
+For effifiency, update rules may pẻom in-place updates, mutating w and
+setting next_w equal to w.
+"""
+
+def sgd(w, dw, config = None):
+    """
+    Performs vanilla stachastic gradient descent.
+    
+    config format:
+    - learning_rate: Scalar learning rate.
+    """
+    if config is None:
+        config = {}
+    config.setdefault("learning_rate", 1e-2)
+
+    w -= config["learning rate"] * dw
+    return w, config

c23c17d6dc3dd6c4861a9cef6bf0f5063c0e4551
TuLa <78714490+TuLa-1031@users.noreply.github.com>
2025-12-12 23:54:45 +0700

    hi

--- minigrad/layer_utils.py
+++ minigrad/layer_utils.py
@@ -25,3 +25,33 @@ def affine_relu_backward(dout, cache):
     da = relu_backward(dout, relu_cache)
     dx, dw, db = affine_backward(da, fc_cache)
     return dx, dw, db
+
+def generic_forward(x, w, b, last=None):
+    """Convenience layer that performs an affine transform, a batch/layer normalization
+
+    Inputs:
+    - x: Input to the affine layer
+    - w, b: Weights for the affine layer
+    - last: Indicates wether to perform just affine forward
+
+    Return a tuple of:
+    - out: Output from the ReLU
+    - cache: Object to give to backward pass
+    """
+    # Initialize optinal cache to None
+    relu_cache = None
+    out, fc_cache = affine_forward(x, w, b)
+    if not last:
+        out, relu_cache = relu_forward(out)
+    cache = fc_cache, relu_cache
+    return out, cache
+
+def generic_backward(dout, cache):
+    """
+    Backward pass for the generic convenience layer
+    """
+    fc_cache, relu_cache = cache
+    if relu_cache is not None:
+        dout = relu_backward(dout, relu_cache)
+    dx, dw, db = affine_backward(dout, fc_cache)
+    return dx, dw, db
--- minigrad/optim.py
+++ minigrad/optim.py
@@ -22,10 +22,10 @@ Returns:
       update rule.
 
 NOTE: for most udpate rules, the defaults learning rate will probably not
-perform well; howeeer the defaults values of the other hyperparameters should
+perform well; however the defaults values of the other hyperparameters should
 work well for a variety of different problems.
 
-For effifiency, update rules may pẻom in-place updates, mutating w and
+For effifiency, update rules may perform in-place updates, mutating w and
 setting next_w equal to w.
 """
 
@@ -42,3 +42,101 @@ def sgd(w, dw, config = None):
 
     w -= config["learning_rate"] * dw
     return w, config
+
+def sgd_momentum(w, dw, config=None):
+    """
+    Performs stochastic gradient descent with momentum.
+
+    config format:
+    - learning_rate: Scalar learning rate.
+    - momentum: Scalar between 0 and 1 giving the momentum value.
+      Setting momentum = 0 reduces to sgd.
+    - velocity: A numpy array of the same shape as w and dw used to store a
+      moving average of the gradients.
+    """
+    if config is None:
+        config = {}
+    config.setdefault("learning_rate", 1e-2)
+    config.setdefault("momentum", 0.9)
+    v = config.get("velocity", np.zeros_like(w))
+
+    next_w = None
+
+    mu = config.get("momentum")
+    lr = config.get("learning_rate")
+    v = mu * v - lr * dw
+    next_w = w + v
+
+    config["velocity"] = v
+    return next_w, config
+
+def rmsprop(w, dw, config=None):
+    """
+    Uses the RMSProp update rule, which uses a moving avrage of squared gradient
+    values to set adaptive per-parameter learning rates.
+
+    config format:
+    - learning_rate: Scalar learning rate.
+    - decay_rate: Scalar between 0 and 1 giving the decay rate for the squared
+      gradient cache.
+    - epsilon: Small scalar used for smoothing to avoid dividing by zero.
+    - cache: Moving avarage of secound moments of gradients.
+    """
+    if config is None:
+        config = {}
+    config.setdefault("learning_rate", 1e-2)
+    config.setdefault("decay_rate", 0.99)
+    config.setdefault("epsilon", 1e-8)
+    config.setdefault("cache", np.zeros_like(w))
+
+    next_w = None
+
+    cache = config["decay_rate"]*config["cache"] + (1-config["decay_rate"])*dw**2
+    next_w = w - config["learning_rate"]*dw/(np.sqrt(cache) + config["epsilon"])
+    config["cache"]=cache
+    return next_w, config
+
+def adam(w, dw, config=None):
+    """
+    Ues the Adam update ruel, which incorporates moving averages of both the gradient
+    and its square and a bias correction term.
+
+    config format:
+    - learning_rate: Scalar learning rate.
+    - beta1: Decay rate for moving average of first moment of gradient.
+    - beta2: Decay rate for moving average of second moment of gradient.
+    - epsilon: Small scalar used for smoothing to avoid divinding by zero.
+    - m: Moving average of gradient.
+    - v: Moving average of squared gradient.
+    - t: Iteration number.
+    """
+    if config is None:
+        config = {}
+    config.setdefault("learning_rate", 1e-3)
+    config.setdefault("beta1", 0.9)
+    config.setdefault("beta2", 0.999)
+    config.setdefault("epsilon", 1e-8)
+    config.setdefault("m", np.zeros_like(w))
+    config.setdefault("v", np.zeros_like(w))
+    config.setdefault("t", 0)
+
+    next_w = None
+    """
+    config["t"] += 1
+    config["m"] = config["beta1"]*config["m"] + (1 - config["beta1"])*dw
+    config["v"] = config["beta2"]*config["v"] + (1-config["beta2"])*dw**2
+    config["learning_rate"] = config["learning_rate"] *                   \
+                              np.sqrt(1-config["beta2"]**config["t"]) /   \
+                              (1-config["beta1"]**config["t"])
+    next_w = w - config["learning_rate"]*config["m"]/(np.sqrt(config["v"]) + config["epsilon"])
+    """
+    keys = ['learning_rate', 'beta1', 'beta2', 'epsilon', 'm', 'v', 't']
+    lr, b1, b2, eps, m, v, t = (config.get(k) for k in keys)
+    config['t'] = t = t + 1
+    config['m'] = m = b1 * m + (1 - b1) * dw
+    config['v'] = v = b2 * v + (1 - b2) * (dw**2)
+    mt = m / (1 - b1**t)
+    vt = v / (1 - b2**t)
+    next_w = w - lr * mt / (np.sqrt(vt) + eps)
+
+    return next_w, config
